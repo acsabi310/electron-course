4ae8b609b26673967c9374e5b3f53904fd8fd05d
Ádám Csaba <adam.csaba.gabor@fornax.hu>
2021-03-16 14:55:05 +0100

    select

--- renderer/items.js
+++ renderer/items.js
@@ -6,11 +6,32 @@ exports.save = () => {
     localStorage.setItem('readit-items', JSON.stringify(this.storage))
 }
 
+exports.changeSelection = direction => {
+    let currentItem = document.getElementsByClassName('read-item selected')[0];
+
+    if (direction === 'ArrowUp' && currentItem.previousElementSibling) {
+        currentItem.classList.remove('selected')
+        currentItem.previousElementSibling.classList.add('selected')
+    } else if(direction === 'ArrowDown' && currentItem.nextElementSibling) {
+        currentItem.classList.remove('selected')
+        currentItem.nextElementSibling.classList.add('selected')
+    }
+}
+
 exports.addItem = (item, isNew = false) => {
     let itemNode = document.createElement('div')
     itemNode.setAttribute('class', 'read-item')
+    itemNode.setAttribute('data-url', item.url)
     itemNode.innerHTML = `<img src="${item.screenshot}"><h2>${item.title}</h2>`
     items.appendChild(itemNode)
+
+    itemNode.addEventListener('click', this.select)
+
+    itemNode.addEventListener('dblclick', this.open)
+
+    if(document.getElementsByClassName('read-item').length === 1) {
+        itemNode.classList.add('selected')
+    }
 
     if (isNew) {
         this.storage.push(item)
@@ -19,6 +40,19 @@ exports.addItem = (item, isNew = false) => {
     
 }
 
+exports.select = e => {
+    document.getElementsByClassName('read-item selected')[0].classList.remove('selected')
+    e.currentTarget.classList.add('selected')
+}
+
+exports.open = () => {
+    if (!this.storage.length) return
+    let selectedItem = document.getElementsByClassName('read-item selected')[0];
+    let contentUrl = selectedItem.dataset.url
+    console.log(contentUrl);
+}
+
 this.storage.forEach(e => {
     this.addItem(e, false)
 })
+
